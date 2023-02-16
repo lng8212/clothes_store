097274715cf2dd84d147885631f8e1334801fcac
Long <long05012001@gmail.com>
2023-02-16 13:03:37 +0700

    add post product

--- clothes/views.py
+++ clothes/views.py
@@ -6,9 +6,16 @@ from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product(request):
     if(request.method == 'GET'):
         snippets = Product.objects.all()
         serializer = ProductSerializer(snippets, many = True)
         return Response(serializer.data)
+    elif(request.method == 'POST'):
+        serializer = ProductSerializer(data = request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif()
