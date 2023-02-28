638aa7e0a53d1a2d6c51ceaf90954754b862795d
Long <long05012001@gmail.com>
2023-02-28 20:20:26 +0700

    add filter by category

--- clothes/urls.py
+++ clothes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views.views import product
+from .views.views import productByCategory
 from .views.cart_views import *
 
 urlpatterns = [
     path('product/', product, name="product"),
-    path('cart/', getCart, name="cart")
+    path('cart/', getCart, name="cart"),
+    path('product/<str:category>/', productByCategory, name='product-by-category'),
 ]
